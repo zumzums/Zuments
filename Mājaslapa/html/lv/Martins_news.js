a0efcc6df4c3a5ec45db1374ed384091180b93e8
zumzums <76159774+zumzums@users.noreply.github.com>
2021-01-04 14:04:04 +0200

    Add files via upload

---
+++ Mājaslapa/html/lv/Martins_news.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var x = document.getElementById("contents");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+var myName="Martins"
+console.log(myName)
